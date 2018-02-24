6d7a5f2edfbb58a52901a4d7fa8aa17664791469
Damian Szocik <damian.szocik@gmail.com>
2018-02-24 16:15:05 +0100

    fixed servie worker scope, v3

--- serviceWorkers.js
+++ serviceWorkers.js
@@ -2,8 +2,8 @@ if (navigator.serviceWorker.controller) {
   console.log('[PWA Builder] active service worker found, no need to register')
 } else {
   //Register the ServiceWorker
-  navigator.serviceWorker.register('pwabuilder-sw.js', {
-    scope: 'ServiceWorker2/'
+  navigator.serviceWorker.register('./ServiceWorker2/pwabuilder-sw.js', {
+    scope: './'
   }).then(function(reg) {
     console.log('Service worker has been registered for scope:'+ reg.scope);
   });
